4cc8a333fa753b921837e103b5ee78a3d3addefc
Your Name <you@example.com>
2022-09-23 18:58:04 +0530

    harshit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :employers
+  devise_for :users
+  resources :applicants
+  
+  # resources :users
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
